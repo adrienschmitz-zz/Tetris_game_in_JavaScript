ff4fc85c830d94156c895134b09f3cba18f0104e
Adrien Schmitz <68816361+adrienschmitz@users.noreply.github.com>
2020-10-23 13:04:09 -0300

    rotação com colisão lateral arrumada

--- tetris.js
+++ tetris.js
@@ -35,6 +35,10 @@ function createMatrix(w, h) {
 function draw() {
     context.fillStyle = '#000';
     context.fillRect(0, 0, canvas.width, canvas.height)
+    drawMatrix(arena, {
+        x: 0,
+        y: 0
+    });
     drawMatrix(player.matrix, player.pos)
 }
 
@@ -70,6 +74,48 @@ function playerDrop() {
     dropCounter = 0
 }
 
+function playerMove(dir) {
+    player.pos.x += dir;
+    if (collide(arena, player)) {
+        player.pos.x -= dir;
+    }
+}
+
+function playerRotate(dir) {
+    const pos = player.pos.x;
+    let offset = 1;
+    rotate(player.matrix, dir);
+    while (collide(arena, player)) {
+        player.pos.x += offset;
+        offset = -(offset + (offset > 0 ? 1 : -1));
+        if (offset > player.matrix[0].length) {
+            rotate(player.matrix, -dir);
+            player.pos.x = pos;
+            return;
+
+        }
+    }
+}
+
+function rotate(matrix, dir) {
+    for (let y = 0; y < matrix.length; ++y) {
+        for (let x = 0; x < y; ++x) {
+            [
+                matrix[x][y],
+                matrix[y][x],
+            ] = [
+                matrix[y][x],
+                matrix[x][y],
+            ];
+        }
+        if (dir > 0) {
+            matrix.forEach(row => row.reverse());
+        } else {
+            matrix.reverse();
+        }
+    }
+}
+
 let dropCounter = 0;
 let dropInterval = 1000;
 
@@ -99,11 +145,15 @@ const player = {
 
 document.addEventListener('keydown', event => {
     if (event.keyCode === 37) {
-        player.pos.x--;
+        playerMove(-1)
     } else if (event.keyCode === 39) {
-        player.pos.x++
+        playerMove(1)
     } else if (event.keyCode === 40) {
         playerDrop();
+    } else if (event.keyCode === 81) {
+        playerRotate(-1)
+    } else if (event.keyCode === 87) {
+        playerRotate(1)
     }
 })
 update();
